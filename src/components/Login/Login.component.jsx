91fcd9325bdcf12d64d960b9cef40abb20604381
daniel.velazquez <danicond97@gmail.com>
2022-01-31 13:49:13 -0600

    feat: auth0 login & logout with history

--- src/components/Login/Login.component.jsx
+++ src/components/Login/Login.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
 import {
@@ -9,10 +9,18 @@ import { useTheme } from '../../providers/Theme/Theme.provider';
 
 function Login() {
   const { theme } = useTheme();
-  const { loginWithRedirect, logout } = useAuth0();
+  const { loginWithRedirect, logout, user } = useAuth0();
 
   return (
     <Container toggle={theme} centered>
+      {user && (
+        <div>
+          asd
+          <img src={user.picture} alt="Profile" />
+          <h3>{user.name}</h3>
+          <h3>{user.email}</h3>
+        </div>
+      )}
       <div>
         <StyledButton onClick={() => loginWithRedirect()}>Log In</StyledButton>
         <StyledButton onClick={() => logout()}>Log Out</StyledButton>
